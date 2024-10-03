94a274dc8840faf3c7c79169a6b0856a420c73ec
Guramrit Singh <guramrits917@gmail.com>
2024-10-03 12:21:38 +0530

    Add files via upload

---
+++ admin/config.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "http://localhost/newswebsite";
+
+$conn = mysqli_connect("localhost","root","","newssite") or die("Connection failed : " . mysqli_connect_error());
+
+?>
---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2024 News | Designed and developed by Guramrit Singh</span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
